f2fcd6a2dc61ea2a60b95a9d935f34b217a48687
Rod <snakepliskin30@gmail.com>
2024-05-26 19:04:24 +0800

    started on the header and navigation section

---
+++ src/app/meals/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const MealInfoPage = ({ params }: { params: { slug: string } }) => {
+  return <div>MealInfoPage: {params.slug}</div>;
+};
+
+export default MealInfoPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import Link from 'next/link';
+
 export default function Home() {
+  console.log('homepage');
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
+        Please click the links below to navigate to other routes
       </h1>
+
+      <ul className='flex flex-col gap-2 items-center mt-5'>
+        <li>
+          <Link href='/meals'>Go to Meals Page</Link>
+        </li>
+        <li>
+          <Link href='/meals/share'>Go to Meals Sharing Page</Link>
+        </li>
+        <li>
+          <Link href='/community'>Go to Community Page</Link>
+        </li>
+        <li>
+          <Link href='/meals/burger'>Go to Burger Meal Page</Link>
+        </li>
+        <li>
+          <Link href='/meals/spaghetti'>Go to Spaghetti Page</Link>
+        </li>
+      </ul>
     </main>
   );
 }
