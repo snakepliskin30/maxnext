223ac836fc8200b1f31d2256f25e7455a639d6b3
Rod <snakepliskin30@gmail.com>
2024-05-27 19:19:08 +0800

    remove the force error for testing in db call for meals

--- src/lib/meals.ts
+++ src/lib/meals.ts
@@ -5,6 +5,5 @@ const db = sql('meals.db');
 
 export const getMeals = async (): Promise<Meal[] | Error> => {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  return new Error('Sinasadyang error');
   return db.prepare('SELECT * FROM meals').all() as Meal[];
 };
